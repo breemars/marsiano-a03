7b963e001e0882b762ae185c3b246931cb9ddc97
Bree <breemars@knights.ucf.edu>
2021-09-26 19:43:17 -0400

    25pseudo

---
+++ exercise25/src/main/java/Solution25.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.util.Scanner;
+
+public class Solution25 {
+    /*
+    Password Strength
+    - input a password
+    - call passwordValidator to count how many numbers, letters, and chars
+    - return an int of the strength of password
+    - use passwordResult to convert int to words
+    - print out result to user
+     */
+
+    public static void main(String[] args) {
+
+        }
+
+}
