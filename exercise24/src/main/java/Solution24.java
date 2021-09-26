266940947683d62345bf339111d3e7fb40c524bc
Bree <breemars@knights.ucf.edu>
2021-09-26 18:29:47 -0400

    24

---
+++ exercise24/src/main/java/Solution24.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution24 {
+    /*
+    Anagram Checker
+    - input two strings
+    - create an array to keep track of letters
+    - convert the strings to char arrays
+    - sort the char arrays alphabetically
+    - if they are equal, the strings are anagrams
+    - print out result to user
+     */
+
+    public static void main(String[] args){
+
+    }
+
+}
