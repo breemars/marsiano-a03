f7d251a9cc576a6a2ddd817f2b5bd779142bb9be
Bree <breemars@knights.ucf.edu>
2021-09-27 01:21:46 -0400

    26pseudo

--- exercise25/src/main/java/Solution25.java
+++ exercise25/src/main/java/Solution25.java
@@ -5,7 +5,7 @@
 
 import java.util.Scanner;
 
-public class Solution25 {
+public class Solution25{
     /*
     Password Strength
     - input a password
@@ -17,6 +17,53 @@ public class Solution25 {
 
     public static void main(String[] args) {
 
+        //INPUT
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a password: ");
+        String password = input.next();
+
+        //OUTPUT
+        System.out.print("The password '" + password + "' has a" + passwordResult(passwordValidator(password)) + " password strength");
+    }
+
+    private static int passwordValidator(String password) {
+        int numCount = 0, letCount = 0, specialCount = 0;
+
+        //Convert to char array for parsing
+        char[] firstSet = password.toUpperCase().toCharArray();
+
+        //Check how many numbers, letters, and special characters
+        for(char z : firstSet){
+            if(z >= '0' & z <= '9')
+                numCount++;
+            else if((z >= 'A' && z <= 'Z') || (z >= 'a' && z <= 'z'))
+                letCount++;
+            else if(z >= '!' & z <= '~')
+                specialCount++;
         }
+
+        //Return strength of password
+        if(firstSet.length < 8 && letCount == 0 && specialCount == 0)
+            return 1; //very weak
+        else if(firstSet.length < 8 && numCount == 0 && specialCount == 0)
+            return 2; //weak
+        else if(firstSet.length >= 8 && numCount >= 1 && letCount >= 2)
+            return 3; //strong
+        else if(firstSet.length >= 8 && numCount >= 1 && letCount >= 1 && specialCount >= 1)
+            return 4; //very strong
+        else
+            return 0; //unknown
+
+    }
+
+    private static String passwordResult(int result){
+        return switch (result) {
+            case 1 -> " very weak";
+            case 2 -> " weak";
+            case 3 -> " strong";
+            case 4 -> " very strong";
+            default -> "n unknown";
+        };
+    }
 
 }
---
+++ exercise26/src/main/java/Solution26.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.util.Scanner;
+
+public class Solution26 extends PaymentCalculator{
+    /*
+    Months to pay off a credit card
+    - input user balance, apr, and monthly payment
+    - calculate daily rate
+    - create PaymentCalculator object
+    - call calculateMonthsUntilPaidOff function in the PaymentCalculator class
+    - output result in months
+     */
+
+    public static void main(String[] args) {
+
+        }
+
+
+
+
+}
