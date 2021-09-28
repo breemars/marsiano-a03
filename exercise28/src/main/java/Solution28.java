f21be26c238e0331624725f1ede22279251accc2
Bree <breemars@knights.ucf.edu>
2021-09-28 14:57:14 -0400

    28 and 29 pseudo

--- exercise28/src/main/java/Solution28.java
+++ exercise28/src/main/java/Solution28.java
@@ -12,5 +12,22 @@ public class Solution28 {
     - Add each num to a total sum
     - output total
     */
+    public static void main(String[] args) {
+
+        int total = getTotal();
+        System.out.print("The total is " + total);
+
+    }
+
+    //Returns total of 5 inputted numbers
+    private static int getTotal(){
+        int total = 0;
+        Scanner input = new Scanner(System.in);
+        for(int x = 0; x < 5; x++){
+            System.out.print("Enter a number: ");
+            total += input.nextInt();
+        }
+        return total;
+    }
 
 }
---
+++ exercise29/src/main/java/Solution29.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.util.Scanner;
+
+public class Solution29 {
+    /*
+    Handling bad input
+    - Try inputting a number from user
+    - catch any mismatch or arithmetic exceptions
+    - allow the user to input values until a valid one is found
+    - print out total years
+    */
+    public static void main(String[] args) {
+       
+    }
+
+}
