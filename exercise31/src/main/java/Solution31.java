f3329eca228fd44a3e262e5b7189fd6bda9ea1a6
Bree <breemars@knights.ucf.edu>
2021-09-30 14:45:55 -0400

    31 and 32 pseudo

--- exercise31/src/main/java/Solution31.java
+++ exercise31/src/main/java/Solution31.java
@@ -12,5 +12,24 @@ public class Solution31 {
     - Call function to print out table of intensities and rates using a loop and incrementing by 5
     - use formula TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
     */
-    
+    public static void main(String[] args) {
+        //INPUT
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your resting pulse? ");
+        int restRate = input.nextInt();
+        System.out.print("What is your age? ");
+        int age = input.nextInt();
+
+        printTable(restRate, age);
+    }
+
+    //OUTPUT - Prints Intensities and Rates
+    private static void printTable(int restRate, int age){
+        System.out.println("\nResting Pulse: " + restRate + "     Age: " + age);
+        System.out.println("Intensity |  Rate\n----------|-----------");
+        for(int i = 55; i <= 95; i += 5) {
+            double targetHeartRate = (((220 - age) - restRate) * (i/100.0)) + restRate;
+            System.out.printf("%4d%%     |  %4.0f bpm %n", i, targetHeartRate);
+        }
+    }
 }
---
+++ exercise32/src/main/java/Solution32.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.util.Scanner;
+
+public class Solution32 {
+    /*
+    Guess the number
+    - Ask the user for level of difficulty
+    - generate a random number based on difficulty
+    - continue to take in user's guesses until they get it correct
+    - use a try catch block to get non-numerical values and count as incorrect
+    - tell them if their guess is too high or low
+    - ask the user if they would like to play again
+    -- if yes, repeat all previous steps
+    -- if no, exit
+    */
+    
+}
