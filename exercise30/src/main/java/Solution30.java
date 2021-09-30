206fbf8753c17f00a6aae2ec7eaa27a88c384109
Bree <breemars@knights.ucf.edu>
2021-09-30 14:15:52 -0400

    30

--- exercise30/src/main/java/Solution30.java
+++ exercise30/src/main/java/Solution30.java
@@ -11,5 +11,16 @@ public class Solution30 {
     - Create a function that will print out a multiplication table using printf for spacing
     - call the function with 1 and 12
     */
-    
+    public static void main(String[] args) {
+        printTable(1, 12);
+    }
+
+    //Prints multiplication table
+    private static void printTable(int a, int b){
+        for(int x = a; x <= b; x++) {
+            for (int y = a; y <= b; y++)
+                System.out.printf("%4d ", x * y);
+            System.out.print("\n");
+        }
+    }
 }
