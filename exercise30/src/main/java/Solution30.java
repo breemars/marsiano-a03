2f54056237d0fa790718b2c22fd19c14e8a2b2a0
Bree <breemars@knights.ucf.edu>
2021-09-30 14:11:19 -0400

    30pseudo

---
+++ exercise30/src/main/java/Solution30.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.util.Scanner;
+
+public class Solution30 {
+    /*
+    Multiplication Table
+    - Create a function that will print out a multiplication table using printf for spacing
+    - call the function with 1 and 12
+    */
+    
+}
