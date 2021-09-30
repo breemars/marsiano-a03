1903cef0963b01874831250787bc4a67b2e3e4be
Bree <breemars@knights.ucf.edu>
2021-09-30 15:23:20 -0400

    32

--- exercise32/src/main/java/Solution32.java
+++ exercise32/src/main/java/Solution32.java
@@ -17,5 +17,62 @@ public class Solution32 {
     -- if yes, repeat all previous steps
     -- if no, exit
     */
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while(true){
+            //Calls the game then outputs results when won
+            System.out.println("You got it in " + playGame() + " guesses!");
+
+            //Asks user if they would like to continue
+            System.out.print("Do you wish to play again (Y/N)? ");
+            String again = input.next();
+            if(!(again.equalsIgnoreCase("y")))
+                break;
+        }
+    }
+
+    //Begins the game
+    private static int playGame(){
+        //INPUT level
+        System.out.println("\nLet's play Guess the Number! ");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the difficulty level (1, 2, or 3): ");
+        int level = input.nextInt();
+
+        //Generate Random Number
+        int num = getNumber(level);
+
+        //INPUT Guesses
+        int count = 0;
+        while(true) {
+            try {
+                System.out.print("Your Guess: ");
+                int guess = Integer.parseInt(input.next());
+                count++;
+
+                if (guess < num)
+                    System.out.print("Too low. ");
+                else if (guess > num)
+                    System.out.print("Too high. ");
+                else
+                    return count;
+
+            } catch (Exception e) {
+                count++;
+                System.out.println("Invalid input. Guesses must be numeric integers.");
+
+            }
+        }
+    }
+
+    //Returns random number based on level
+    private static int getNumber(int level) {
+        return switch(level){
+            case 1 -> (int)(Math.random() * 10) + 1;
+            case 2 -> (int)(Math.random() * 100) + 1;
+            case 3 -> (int)(Math.random() * 1000) + 1;
+            default -> throw new IllegalStateException("Invalid value: " + level);
+        };
+    }
 }
