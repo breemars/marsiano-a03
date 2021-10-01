cf66505893a99a58f57fd86fce10de950585c38d
Bree <breemars@knights.ucf.edu>
2021-10-01 14:52:51 -0400

    34 and 35p

--- exercise34/src/main/java/Solution34.java
+++ exercise34/src/main/java/Solution34.java
@@ -15,6 +15,39 @@ public class Solution34 {
     - remove employee from list
     - reprint out the list
     */
-    
+    public static void main(String[] args) {
+
+        //Create and print list
+        List<String> list = getList();
+        printList(list);
+
+        //Get employee to remove
+        Scanner input = new Scanner(System.in);
+        System.out.print("\nEnter an employee name to remove: ");
+        String remove = input.nextLine();
+
+        //Remove and reprint
+        list.remove(remove);
+        printList(list);
+    }
+
+    //Prints list
+    private static void printList(List<String> list) {
+        System.out.println("\nThere are " + list.size() + " employees: ");
+        for(String employee : list)
+            System.out.println(employee);
+    }
+
+    //Creates list
+    private static List<String> getList() {
+        List<String> list = new ArrayList<>();
+        list.add("Captain America");
+        list.add("Iron Man");
+        list.add("Black Widow");
+        list.add("Hawkeye");
+        list.add("Thor of Asgard");
+        list.add("The Hulk");
+        return list;
+    }
 
 }
---
+++ exercise35/src/main/java/Solution35.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution35 {
+    /*
+    Picking a winner
+    - Create an empty list
+    - keep on asking user for names and add them to list
+    - when user inputs " ", end the loop
+    - generate random number based on length of list
+    - print out name using random number, who will be the winner
+    */
+    
+}
