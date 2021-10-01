1d6f153625f72e42d8258902f5b9a10b7ea71175
Bree <breemars@knights.ucf.edu>
2021-10-01 17:12:38 -0400

    36 and 37p

--- exercise36/src/main/java/Solution36.java
+++ exercise36/src/main/java/Solution36.java
@@ -16,5 +16,74 @@ public class Solution36 {
     - compute av, max, min, and standard dev in separate functions
     - output results
     */
-    
+    public static void main(String[] args) {
+
+        //INPUT - Creates list based on user input
+        List<Double> list = getList();
+
+        //OUTPUT - Calculate and print results
+        System.out.printf("The average is %.1f", average(list));
+        System.out.printf("\nThe minimum is %.0f", min(list));
+        System.out.printf("\nThe maximum is %.0f", max(list));
+        System.out.printf("\nThe standard deviation is %.2f", std(list, average(list)));
+    }
+
+    //Returns standard deviation
+    private static double std(List<Double> list, double average) {
+        double standardDeviation = 0.0;
+
+        for(double num: list)
+            standardDeviation += Math.pow(num - average, 2);
+
+        return Math.sqrt(standardDeviation/list.size());
+    }
+
+    //Returns maximum
+    private static double max(List<Double> list) {
+        double maximum = list.get(0);
+        for(double num : list)
+            if(num > maximum)
+                maximum = num;
+        return maximum;
+    }
+
+    //Returns minimum
+    private static double min(List<Double> list) {
+        double minimum = list.get(0);
+        for(double num : list)
+            if(num < minimum)
+                minimum = num;
+        return minimum;
+    }
+
+    //Returns average
+    private static double average(List<Double> list) {
+        double total = 0;
+        for(double num : list)
+            total += num;
+        return total/list.size();
+    }
+
+    //Creates list
+    private static List<Double> getList() {
+        List<Double> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        //Get numbers
+        while(true){
+            System.out.print("Enter a number: ");
+            String in = input.next();
+            if((in.equals("done")))
+                break;
+
+            try{
+                double num = Double.parseDouble(in);
+                list.add(num);
+            }catch(Exception e){
+                System.out.print("Invalid Input. Enter a number or \"done\" ");
+            }
+        }
+        return list;
+    }
+
 }
---
+++ exercise37/src/main/java/Solution37.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution37 {
+    /*
+    Password Generator
+    - input minimum length, special char count, and number count
+    - use a separate class to store methods as this is a long program that belongs in object-format
+    -- it will be more organized/easier to set the char arrays and call the calculations from a separate class
+    - create 3 lists of possible letters, numbers, and special chars
+    - while password incomplete:
+    -- generate a random number for type of char going to be inserted
+    -- generate another random number to pick from that type of list
+    -- insert that char into a final char list for the final password
+    - return the final password and print
+    */
+
+}
