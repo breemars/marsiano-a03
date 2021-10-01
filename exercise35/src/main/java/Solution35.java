d081263fb19d0760dcc50b844a700a6280ccbe39
Bree <breemars@knights.ucf.edu>
2021-10-01 15:25:03 -0400

    35 and 36p

--- exercise35/src/main/java/Solution35.java
+++ exercise35/src/main/java/Solution35.java
@@ -16,5 +16,30 @@ public class Solution35 {
     - generate random number based on length of list
     - print out name using random number, who will be the winner
     */
-    
+    public static void main(String[] args) {
+
+        //Creates list based on user input
+        List<String> list = getList();
+
+        //Prints winner
+        int num = (int)(Math.random() * list.size());
+        System.out.println("The winner is... " + list.get(num));
+    }
+
+    //Creates list
+    private static List<String> getList() {
+        List<String> list = new ArrayList<>();
+
+        //Get names
+        while(true){
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter a name: ");
+            String name = input.nextLine();
+            if((name.equals(" ")))
+                break;
+            list.add(name);
+        }
+        return list;
+    }
+
 }
---
+++ exercise36/src/main/java/Solution36.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution36 {
+    /*
+    Statistics
+    - input numbers until user enters "done"
+    - use try and catch to filter invalid info
+    - store numbers in arraylist
+    - compute av, max, min, and standard dev in separate functions
+    - output results
+    */
+    
+}
