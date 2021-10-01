b07eeba29026a7cc1cc9acef06d7a57c8f25f55f
Bree <breemars@knights.ucf.edu>
2021-10-01 14:05:30 -0400

    33ps

---
+++ exercise33/src/main/java/Solution33.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution33 {
+    /*
+    Magic 8 Ball
+    - prompt the user for a question
+    - generate a random number between 1-4
+    - store possible responses in a list
+    - display the answer based on the number
+    */
+   
+}
