91b93109ab8d44322e6be8b13a8ae9ce076071d0
Bree <breemars@knights.ucf.edu>
2021-10-02 16:36:54 -0400

    39ps

--- exercise39/src/main/java/Solution39.java
+++ exercise39/src/main/java/Solution39.java
@@ -1,10 +1,17 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Breanna Marsiano
+ */
+import java.util.*;
 
 public class Solution39 {
-    public static void main(String[] args) {
-
-
-    }
+    /*
+    Sorting Records
+    - Create a list to store the employees
+    - Create a map for each employee that stores the employee's data by first, last, position, and date keys
+    - Add each of the maps to the list
+    - sort list by the last name of each map
+    - print out in table format
+    */
+    
 }
